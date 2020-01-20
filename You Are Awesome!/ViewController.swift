feabe8e9698745bd3961678f02ba342404880867
Darwie Fang <fangdb@bc.edu>
2020-01-20 12:14:36 -0500

    2.5 Adding images to file

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func messageLabelPressed(_ sender: UIButton) {
+        messageLabel.text = "You Are Fabulous!"
+  
+        imageView.image = UIImage (named: "image0")
+    }
+    
+    @IBOutlet weak var messageLabel: UILabel!
 }
 
