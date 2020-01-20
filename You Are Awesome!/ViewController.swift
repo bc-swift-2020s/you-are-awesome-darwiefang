4aafeccc185fb91665b920e306c67a2b96386cd9
Darwie Fang <fangdb@bc.edu>
2020-01-20 16:45:24 -0500

    2.6 autoresizing

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
    
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageLabel.text = ""
         // Do any additional setup after loading the view.
     }
 
