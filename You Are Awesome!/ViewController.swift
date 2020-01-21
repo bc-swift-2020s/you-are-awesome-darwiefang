642539e9177b1dd1892d6e67aa1d42770b8db5d3
Darwie Fang <fangdb@bc.edu>
2020-01-21 10:47:38 -0500

    2.9 working with Xcode playground and adding messages

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
   var imageNumber = 0
+    var messageNumber = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +22,17 @@ class ViewController: UIViewController {
     }
     
     @IBAction func messageLabelPressed(_ sender: UIButton) {
-    
-        print(imageNumber)
+        let messages = ["Lit",
+                    "Giddyup",
+                    "Send it",
+                    "Nice"]
+        
+        messageLabel.text = messages[messageNumber]
+        messageNumber += 1
+        if messageNumber == messages.count {
+        messageNumber = 0
+        }
+
         // let imageName = "image" + String(imageNumber)
        let imageName = "image\(imageNumber)"
         imageView.image = UIImage(named: imageName)
